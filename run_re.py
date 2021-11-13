2e01028bf00100b09178c93a526162720ad87aed
jolsh413 <87673243+jolsh413@users.noreply.github.com>
2021-11-12 19:13:27 -0500

    Update run_re.py

--- run_re.py
+++ run_re.py
@@ -1961,7 +1961,7 @@ def main(_):
       "drugprot": BioBERTDrugProtProcessor,
       "ddi13": BioBERTDDIProcessor,
       "aimed": BioBERTProcessor,
-      "reu" : BioBERTProcessor_REU
+      "reu-tpu" : BioBERTProcessor_REU
   }
   models = {
       "original": create_model,
