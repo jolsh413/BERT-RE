5d2ebfdcb0153f18d939623892ae6c887dacd295
jolsh413 <87673243+jolsh413@users.noreply.github.com>
2021-12-29 15:38:35 -0500

    Update run_model_tpu.sh

--- run_model_tpu.sh
+++ run_model_tpu.sh
@@ -1,5 +1,5 @@
 TASK_NAME="reu-tpu"
-STORAGE_BUCKET=gs://reu_file6
+STORAGE_BUCKET=gs://reu_file10
 BERT_BASE_DIR=$STORAGE_BUCKET/biobert_v1.1_pubmed
 
 DATA_DIR=$STORAGE_BUCKET/REU_google_cloud_biobert081221
@@ -9,7 +9,7 @@ for s in 2 4 6 8 10
 	do
 	for i in {1..10}
 		do
-			python3 run_re.py --task_name=$TASK_NAME --do_train=true --do_eval=false --do_predict=true --vocab_file=$BERT_BASE_DIR/vocab.txt --bert_config_file=$BERT_BASE_DIR/bert_config.json --init_checkpoint=$BERT_BASE_DIR/model.ckpt-1000000 --max_seq_length=128 --train_batch_size=32 --learning_rate=2e-5 --num_train_epochs=${s} --do_lower_case=false --data_dir=${DATA_DIR}${i} --output_dir=${OUTPUT_DIR}${i} --use_tpu=True --tpu_name=reu-phen2
+			python3 run_re.py --task_name=$TASK_NAME --do_train=true --do_eval=false --do_predict=true --vocab_file=$BERT_BASE_DIR/vocab.txt --bert_config_file=$BERT_BASE_DIR/bert_config.json --init_checkpoint=$BERT_BASE_DIR/model.ckpt-1000000 --max_seq_length=128 --train_batch_size=32 --learning_rate=2e-5 --num_train_epochs=${s} --do_lower_case=false --data_dir=${DATA_DIR}${i} --output_dir=${OUTPUT_DIR}${i} --use_tpu=True --tpu_name=reu-phen10
 
 
 	done
