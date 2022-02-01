49e3cb18378f2f6098bb82e538a47f8751e89bce
jolsh413 <87673243+jolsh413@users.noreply.github.com>
2022-02-01 15:38:16 -0500

    Update run_model2_tpu.sh

--- run_model2_tpu.sh
+++ run_model2_tpu.sh
@@ -1,5 +1,5 @@
 TASK_NAME="reu-tpu"
-STORAGE_BUCKET=gs://reu_file15
+STORAGE_BUCKET=gs://reu_15
 BERT_BASE_DIR=$STORAGE_BUCKET/biobert_v1.1_pubmed
 
 DATA_DIR=$STORAGE_BUCKET/REU_google_cloud_biobertBP/
