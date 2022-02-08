cf31337a67d6aa2cdfafafd1dd3d8e077464e0db
jolsh413 <87673243+jolsh413@users.noreply.github.com>
2022-02-07 20:18:46 -0500

    Update run_model2_tpu.sh

--- run_model2_tpu.sh
+++ run_model2_tpu.sh
@@ -9,7 +9,7 @@ for s in 2 4 6 8 10
 	do
 	for i in {1..10}
 		do
-			python3 run_re.py --task_name=$TASK_NAME --do_train=true --do_eval=false --do_predict=true --vocab_file=$BERT_BASE_DIR/vocab.txt --bert_config_file=$BERT_BASE_DIR/bert_config.json --init_checkpoint=$BERT_BASE_DIR/model.ckpt-1000000 --max_seq_length=512 --train_batch_size=32 --learning_rate=2e-5 --num_train_epochs=${s} --do_lower_case=false --data_dir=${DATA_DIR}${i} --output_dir=${OUTPUT_DIR}${i} --use_tpu=True --tpu_name=reu-phen
+			python3 run_re.py --task_name=$TASK_NAME --do_train=true --do_eval=false --do_predict=true --vocab_file=$BERT_BASE_DIR/vocab.txt --bert_config_file=$BERT_BASE_DIR/bert_config.json --init_checkpoint=$BERT_BASE_DIR/model.ckpt-1000000 --max_seq_length=256 --train_batch_size=32 --learning_rate=2e-5 --num_train_epochs=${s} --do_lower_case=false --data_dir=${DATA_DIR}${i} --output_dir=${OUTPUT_DIR}${i} --use_tpu=True --tpu_name=reu-phen
 
 
 	done
